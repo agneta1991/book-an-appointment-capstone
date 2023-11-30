03ce1300fd97c45272562868a78f37eda9f7850c
agneta1991 <pupienytea@gmail.com>
2023-11-30 14:43:35 +0200

    Added info to seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,146 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Doctor.create!(
+    id: 1,
+  name: 'Jessica Smith',
+  img: '',
+  specialization: 'Dermatology',
+  years_of_experience: 7,
+  price_per_appointment: 250
+)
+
+Doctor.create!(
+    id: 2,
+  name: 'Robert Jameson',
+  img: '',
+  specialization: 'Family Medicine',
+  years_of_experience: 12,
+  price_per_appointment: 200
+)
+
+Doctor.create!(
+    id: 3,
+  name: 'Gabriel Lloyd',
+  img: '',
+  specialization: 'Pediatrics',
+  years_of_experience: 14,
+  price_per_appointment: 180
+)
+
+Doctor.create!(
+    id: 4,
+  name: 'Aurora Bruit',
+  img: '',
+  specialization: 'Obstetrics and Gynecology (OB/GYN)',
+  years_of_experience: 4,
+  price_per_appointment: 300
+)
+
+Doctor.create!(
+    id: 5,
+  name: 'Michael D. Valentine',
+  img: '',
+  specialization: 'Orthopedics',
+  years_of_experience: 9,
+  price_per_appointment: 300
+)
+
+User.create!(
+    id: 1,
+  email: 'user1@example.com',
+  password: 'password1',
+  name: 'User One',
+  admin: false
+)
+
+User.create!(
+    id: 2,
+  email: 'user2@example.com',
+  password: 'password2',
+  name: 'User Two',
+  admin: false
+)
+
+User.create!(
+    id: 3,
+  email: 'user3@example.com',
+  password: 'password3',
+  name: 'User Three',
+  admin: false
+)
+
+User.create!(
+    id: 4,
+  email: 'admin1@example.com',
+  password: 'adminpassword1',
+  name: 'Admin One',
+  admin: true
+)
+
+User.create!(
+    id: 5,
+  email: 'admin2@example.com',
+  password: 'adminpassword2',
+  name: 'Admin Two',
+  admin: true
+)
+
+User.create!(
+    id: 6,
+  email: 'admin3@example.com',
+  password: 'adminpassword3',
+  name: 'Admin Three',
+  admin: true
+)
+
+Reservation.create!(
+  id: 1,
+  time: '14:20',
+  date: '30/12/2023',
+  user_id: 1,
+  doctor_id: 1,
+  description: 'Description for Reservation 1'
+)
+
+Reservation.create!(
+  id: 2,
+  time: '15:20',
+  date: '30/12/2023',
+  user_id: 2,
+  doctor_id: 1,
+  description: 'Description for Reservation 2'
+)
+
+Reservation.create!(
+  id: 3,
+  time: '16:20',
+  date: '30/12/2023',
+  user_id: 3,
+  doctor_id: 1,
+  description: 'Description for Reservation 3'
+)
+
+Reservation.create!(
+  id: 4,
+  time: '14:20',
+  date: '01/01/2024',
+  user_id: 4,
+  doctor_id: 2,
+  description: 'Description for Reservation 4'
+)
+
+Reservation.create!(
+  id: 5,
+  time: '15:20',
+  date: '01/01/2024',
+  user_id: 5,
+  doctor_id: 2,
+  description: 'Description for Reservation 5'
+)
+
+Reservation.create!(
+  id: 6,
+  time: '16:20',
+  date: '01/01/2024',
+  user_id: 6,
+  doctor_id: 2,
+  description: 'Description for Reservation 6'
+)
