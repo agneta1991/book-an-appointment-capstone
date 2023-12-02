5673884bb79fc569567e9295c534eb71ac83d7df
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-12-02 17:49:33 +0300

    fixed rubocop errors

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,50 +1,48 @@
 class Api::V1::UsersController < ApplicationController
-    before_action :set_user, only: [:show, :update, :create]
+  before_action :set_user, only: %i[show update create]
 
-    # GET /api/v1/users
-    def index
-      @users = User.all
-      render json: @users
+  # GET /api/v1/users
+  def index
+    @users = User.all
+    render json: @users
+  end
+
+  # GET /api/v1/users/1
+  def show
+    render json: @user
+  end
+
+  # POST /api/v1/users
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
-  
-    # GET /api/v1/users/1
-    def show
+  end
+
+  # PATCH/PUT /api/v1/users/1
+  def update
+    if @user.update(user_params)
       render json: @user
+    else
+      render json: @user.errors, status: :unprocessable_entity
     end
-  
-    # POST /api/v1/users
-    def create
-      @user = User.new(user_params)
-  
-      if @user.save
-        render json: @user, status: :created
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
-    end
-  
-    # PATCH/PUT /api/v1/users/1
-    def update
-      if @user.update(user_params)
-        render json: @user
-      else
-        render json: @user.errors, status: :unprocessable_entity
-      end
-    end
-    
-  
-    private
-  
-    def set_user
-      @user = User.find(params[:id])
-    end
-  
-    def sign_up_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-  
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
-    end
-    
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def sign_up_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
