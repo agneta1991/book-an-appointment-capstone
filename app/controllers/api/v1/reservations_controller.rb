3d61182a1ade106124c129b056087311e7ad4671
Bhekisile <bhekisilejozi@gmail.com>
2023-12-02 17:34:30 +0200

    Create index, show and create actions

--- app/controllers/api/v1/reservations_controller.rb
+++ app/controllers/api/v1/reservations_controller.rb
@@ -1,42 +1,27 @@
 class Api::V1::ReservationsController < ApplicationController
   def index
-    @reservation = Reservation.all
-    render json: @reservations
+    current_user_id = params[:user_id]
+    user = User.find(current_user_id)
+    render json: { reservations: @reservation }
   end
 
-  def new
-    @reservation = Reservation.new
+  def show
+    render json: @reservation
   end
   
   def create
-    @reservation = @current_user.appointments.build(appointment_params)
-    
+    @reservation = Reservation.new(reservation_params)
+
     if @reservation.save
-      render json: reservation, status: :created
+      render json: @reservation, status: :created, location: @reservation
     else
-      render json: reservation.errors, status: :unprocessable_entity
+      render json: @reservation.errors, status: :unprocessable_entity
     end
-  end
-
-  def update
-    respond_to do |format|
-      if @reservation.update(reservation_params)
-        render json: reservation, status: :updated
-      else
-        render json: reservation.errors, status: :unprocessable_entity
-  end
-
-  def destroy
-    @reservation.destroy
-
-    respond_to do |format|
-      render json: reservation, status: :destroyed
-      render json: reservation.errors, status: :unprocessable_entity 
   end
 
   private
 
   def reservation_params
-    params.require(:reservation).permit(:reservation_id, :doctor_id, :time, :date, :text)
+    params.require(:reservation).permit(:reservation_id, :doctor_id, :date)
   end
 end
