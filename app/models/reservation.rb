135b258962ad72b9cd58e4b893f38dd299683c93
agneta1991 <pupienytea@gmail.com>
2023-12-05 19:16:19 +0200

    Added tests for reservation requests

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :user, dependent: :destroy
   belongs_to :doctor, dependent: :destroy
+  has_many :dependent_association, dependent: :destroy
+
 
   validates :user, presence: true
   validates :date, presence: true
