abec5d5afd4ef8e688f748d3dbf63fad06dec358
agneta1991 <pupienytea@gmail.com>
2023-12-07 15:20:12 +0200

    cors issue fixed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 admin1 = User.create!(
-  email: 'admin1@example.com',
+  email: 'admin1@example1.com',
   password: 'adminpassword1',
   password_confirmation: 'adminpassword1',
   name: 'Yousef',
@@ -7,7 +7,7 @@ admin1 = User.create!(
 )
 
 admin2 = User.create!(
-  email: 'admin2@example.com',
+  email: 'admin2@example2.com',
   password: 'adminpassword2',
   password_confirmation: 'adminpassword2',
   name: 'Bwambale',
@@ -15,7 +15,7 @@ admin2 = User.create!(
 )
 
 admin3 = User.create!(
-  email: 'admin3@example.com',
+  email: 'admin3@example3.com',
   password: 'adminpassword3',
   password_confirmation: 'adminpassword3',
   name: 'Wolter',
@@ -23,7 +23,7 @@ admin3 = User.create!(
 )
 
 user1 = User.create!(
-  email: 'user1@example.com',
+  email: 'user1@example4.com',
   password: 'password1',
   password_confirmation: 'password1',
   name: 'Agneta',
@@ -31,7 +31,7 @@ user1 = User.create!(
 )
 
 user2 = User.create!(
-  email: 'user2@example.com',
+  email: 'user2@example5.com',
   password: 'password2',
   password_confirmation: 'password2',
   name: 'Jozi',
@@ -39,7 +39,7 @@ user2 = User.create!(
 )
 
 user3 = User.create!(
-  email: 'user3@example.com',
+  email: 'user3@example6.com',
   password: 'password3',
   password_confirmation: 'password3',
   name: 'Arnold',
@@ -53,8 +53,8 @@ doctor1 = Doctor.create!(
   years_of_experience: 7,
   price_per_appointment: 250,
   user_id: admin1.id,
-  img: ''
-)
+  img: 'https://images.pexels.com/photos/3714743/pexels-photo-3714743.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  )
 
 doctor2 = Doctor.create!(
   id: 2,
@@ -63,18 +63,18 @@ doctor2 = Doctor.create!(
   years_of_experience: 12,
   price_per_appointment: 200,
   user_id: admin1.id,
-  img: ''
-)
+  img: 'https://images.pexels.com/photos/8460094/pexels-photo-8460094.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  )
 
 doctor3 = Doctor.create!(
   id: 3,
   name: 'Gabriel Lloyd',
   specialization: 'Pediatrics',
-  years_of_experience: 14,
+  years_of_experience: 5,
   price_per_appointment: 180,
   user_id: admin1.id,
-  img: ''
-)
+  img: 'https://images.pexels.com/photos/6627931/pexels-photo-6627931.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  )
 
 doctor4 = Doctor.create!(
   id: 4,
@@ -83,8 +83,8 @@ doctor4 = Doctor.create!(
   years_of_experience: 4,
   price_per_appointment: 300,
   user_id: admin1.id,
-  img: ''
-)
+  img: 'https://images.pexels.com/photos/5215024/pexels-photo-5215024.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  )
 
 doctor5 = Doctor.create!(
   id: 5,
@@ -93,8 +93,8 @@ doctor5 = Doctor.create!(
   years_of_experience: 9,
   price_per_appointment: 300,
   user_id: admin1.id,
-  img: ''
-)
+  img: 'https://images.pexels.com/photos/5327656/pexels-photo-5327656.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'
+  )
 
 reservation1 = Reservation.create!(
   id: 1,
